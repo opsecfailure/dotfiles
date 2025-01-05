baec90321d3ce2d64f96b9fb7f4a7f12b9e6d740
Mehmet Salih Erken <opsecfail@opsec.fail>
2025-01-05 13:37:36 +0300

    move .bash_profile to .profile
    various qutebrowser and sway changes

--- .config/qutebrowser/bindings.py
+++ .config/qutebrowser/bindings.py
@@ -1,35 +1,42 @@
-config.unbind('<Ctrl+q>')
-config.unbind('co')
-config.unbind('m')
+config.unbind("<Ctrl+q>")
+config.unbind("co")
+config.unbind("m")
 
-config.bind('!',  'cmd-set-text :open -t -r !')
-config.bind('A',  'cmd-set-text --space :open -t')
-config.bind('O',  'cmd-set-text --space :open -t -r')
+config.bind("!",  "cmd-set-text :open -t -r !")
+config.bind("A",  "cmd-set-text --space :open -t")
+config.bind("O",  "cmd-set-text --space :open -t -r")
 
-config.bind('ma', 'bookmark-add')
-config.bind('md', 'bookmark-del')
-config.bind('mq', 'quickmark-save')
+config.bind("ma", "bookmark-add")
+config.bind("md", "bookmark-del")
+config.bind("mq", "quickmark-save")
 
-config.bind('Y',  'hint links spawn wl-copy {hint-url}')
+config.bind("za", "config-cycle tabs.show always never")
+config.bind("zs", "config-cycle statusbar.show always never")
+config.bind("zz", (
+    "config-cycle tabs.show always never "
+    ";; config-cycle statusbar.show always never"
+))
 
-config.bind('M',  'hint links spawn --detach oits media mpv {hint-url}')
-config.bind(',M', 'spawn --detach oits media mpv {url}')
+config.bind("Y",  "hint links spawn wl-copy {hint-url}")
 
-config.bind('aM',  'hint links spawn --detach oits media mpv --no-video {hint-url}')
-config.bind(',aM', 'spawn --detach oits media mpv {url}')
+config.bind("M",  "hint links spawn --detach oits media mpv {hint-url}")
+config.bind(",M", "spawn --detach oits media mpv {url}")
 
-config.bind('wY',  'spawn --detach oits dl yt-dlp {url}')
-config.bind('wy',  'hint links spawn --detach oits dl yt-dlp {hint-url}')
-config.bind(',wy', 'hint --rapid links spawn --detach oits dl yt-dlp {hint-url}')
+config.bind("aM",  "hint links spawn --detach oits media mpv --no-video {hint-url}")
+config.bind(",aM", "spawn --detach oits media mpv {url}")
 
-config.bind('wE',  'spawn --detach oits dl yt-dlp-music {url}')
-config.bind('we',  'hint links spawn --detach oits dl yt-dlp-music {hint-url}')
-config.bind(',we', 'hint --rapid links spawn --detach oits dl yt-dlp-music {hint-url}')
+config.bind("wY",  "spawn --detach oits dl yt-dlp {url}")
+config.bind("wy",  "hint links spawn --detach oits dl yt-dlp {hint-url}")
+config.bind(",wy", "hint --rapid links spawn --detach oits dl yt-dlp {hint-url}")
 
-config.bind('wA',  'spawn --detach oits dl yt-dlp-audio {url}')
-config.bind('wa',  'hint links spawn --detach oits dl yt-dlp-audio {hint-url}')
-config.bind(',wa', 'hint --rapid links spawn --detach oits dl yt-dlp-audio {hint-url}')
+config.bind("wE",  "spawn --detach oits dl yt-dlp-music {url}")
+config.bind("we",  "hint links spawn --detach oits dl yt-dlp-music {hint-url}")
+config.bind(",we", "hint --rapid links spawn --detach oits dl yt-dlp-music {hint-url}")
 
-config.bind('wC',  'spawn --detach oits dl curl2dl {url}')
-config.bind('wc',  'hint links spawn --detach oits dl curl2dl {hint-url}')
-config.bind(',wc', 'hint --rapid links spawn --detach oits dl curl2dl {hint-url}')
+config.bind("wA",  "spawn --detach oits dl yt-dlp-audio {url}")
+config.bind("wa",  "hint links spawn --detach oits dl yt-dlp-audio {hint-url}")
+config.bind(",wa", "hint --rapid links spawn --detach oits dl yt-dlp-audio {hint-url}")
+
+config.bind("wC",  "spawn --detach oits dl curl2dl {url}")
+config.bind("wc",  "hint links spawn --detach oits dl curl2dl {hint-url}")
+config.bind(",wc", "hint --rapid links spawn --detach oits dl curl2dl {hint-url}")
--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -4,25 +4,58 @@ c.auto_save.session = True
 c.completion.height = "25%"
 c.completion.show = "auto"
 c.content.autoplay = False
-c.content.blocking.adblock.lists = ["https://easylist.to/easylist/easylist.txt", "https://secure.fanboy.co.nz/fanboy-cookiemonster.txt", "https://easylist.to/easylist/easyprivacy.txt", "https://secure.fanboy.co.nz/fanboy-annoyance.txt", "https://easylist.to/easylist/fanboy-social.txt"]
-c.content.blocking.hosts.lists = ["https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/porn-social/hosts"]
+c.content.blocking.adblock.lists = [
+    "https://easylist.to/easylist/easylist.txt",
+    "https://easylist.to/easylist/easyprivacy.txt",
+    "https://easylist.to/easylist/fanboy-social.txt",
+    "https://secure.fanboy.co.nz/fanboy-annoyance.txt",
+    "https://secure.fanboy.co.nz/fanboy-cookiemonster.txt",
+]
+c.content.blocking.hosts.lists = [
+    "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/porn/hosts"
+]
 c.content.cookies.accept = "no-3rdparty"
 c.content.geolocation = False
 c.content.mouse_lock = False
 c.content.notifications.enabled = False
 c.downloads.location.remember = False
 c.downloads.prevent_mixed_content = True
-c.editor.command = ['foot', '--app-id=qutebrowser_editor', 'nvim', '{file}', '-c' 'normal {line}G{column0}l']
-c.fileselect.folder.command = ['foot', '--app-id=qutebrowser_fileselect', 'lf', '-selection-path', '{}']
+c.editor.command = [
+    "foot",
+    "--app-id=qutebrowser_editor",
+    "nvim",
+    "{file}",
+    "-c",
+    "normal {line}G{column0}l"
+]
+c.fileselect.folder.command = [
+    "foot",
+    "--app-id=qutebrowser_fileselect",
+    "lf",
+    "-selection-path",
+    "{}"
+]
 c.fileselect.handler = "external"
-c.fileselect.multiple_files.command = ['foot', '--app-id=qutebrowser_fileselect', 'lf', '-selection-path', '{}']
-c.fileselect.single_file.command = ['foot', '--app-id=qutebrowser_fileselect', 'lf', '-selection-path', '{}']
+c.fileselect.multiple_files.command = [
+    "foot",
+    "--app-id=qutebrowser_fileselect",
+    "lf",
+    "-selection-path",
+    "{}"
+]
+c.fileselect.single_file.command = [
+    "foot",
+    "--app-id=qutebrowser_fileselect",
+    "lf",
+    "-selection-path",
+    "{}"
+]
 c.fonts.default_family = "GoMono Nerd Font Mono"
-c.fonts.default_size = "15pt"
+c.fonts.default_size = "16pt"
 c.fonts.web.family.fixed = "GoMono Nerd Font Mono"
-c.fonts.web.size.default = 19
-c.fonts.web.size.default_fixed = 19
-c.fonts.web.size.minimum = 19
+c.fonts.web.size.default = 20
+c.fonts.web.size.default_fixed = 20
+c.fonts.web.size.minimum = 20
 c.tabs.close_mouse_button = "none"
 c.tabs.indicator.width = 4
 c.tabs.last_close = "blank"
